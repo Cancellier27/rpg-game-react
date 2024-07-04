fe084b3cdf1bc6b4462c16aeccdc112ae0fff7a8
Cancellier27 <filipecancelliercosta@gmail.com>
2024-07-04 20:02:17 +0100

    update css for the status table

--- src/components/battle/BattleStatusMenu.js
+++ src/components/battle/BattleStatusMenu.js
@@ -5,7 +5,7 @@ import StatusBoard from "./StatusBoard"
 import { useState } from "react"
 
 export default function BattleStatusMenu({level}) {
-  const [isStatusBoard, setIsStatusBoard] = useState(true)
+  const [isStatusBoard, setIsStatusBoard] = useState(false)
 
   function onClickHandler() {
     setIsStatusBoard(true)
--- src/components/battle/StatusBoard.js
+++ src/components/battle/StatusBoard.js
@@ -1,52 +1,64 @@
-import {useEffect} from "react"
 import "./battle.css"
 
-function player(combatant) {
-  return (
-    <div>
-      <p>{combatant.combatant.name}</p>
-      <div>
-        <p>{combatant.combatant.status.type}</p>{" "}
-        <p>{combatant.combatant.status.expiresIn}</p>
-      </div>
-    </div>
-  )
-}
-
 export default function StatusBoard({level, setIsStatusBoard}) {
-  // const status = []
+  const playerStatus = []
+  const enemyStatus = []
 
-  // useEffect(() => {
-  //   const players = level.battle.combatants
+  Object.values(level.battle.combatants).forEach((combatant) => {
+    if (combatant.team === "player" && combatant.status) {
+      playerStatus.push(
+        <div className="status-board-player" key={combatant.name}>
+          <h3 className="status-board-player-name">{combatant.name}</h3>
 
-  //   Object.values(players).forEach((combatant) => {
-  //     if (combatant.team === "player") {
-  //       status.push(<Test combatant={combatant} />)
-  //     }
-  //   })
-  // }, [level])
+          <div className="status-board-player-status">
+            <h4>{combatant.status.type}</h4>{" "}
+            <h3>{combatant.status.expiresIn}</h3>
+          </div>
+        </div>
+      )
+    } else if (combatant.team === "enemy" && combatant.status) {
+      enemyStatus.push(
+        <div className="status-board-player" key={combatant.name}>
+          <h3 className="status-board-player-name">{combatant.name}</h3>
+
+          <div className="status-board-player-status">
+            <h4>{combatant.status.type}</h4>{" "}
+            <h3>{combatant.status.expiresIn}</h3>
+          </div>
+        </div>
+      )
+    }
+  })
+
+  if (playerStatus.length > 0) {
+    playerStatus.push(
+      <div
+        style={{
+          width: "90%",
+          height: "1px",
+          background: "var(--menu-border-color)"
+        }}
+        key={"separator-bar-status-board"}
+      ></div>
+    )
+  }
 
   return (
     <div className="status-board-container">
-      {/* {[status]}
-      <Test combatant={level.battle.combatants["player1"]} /> */}
       <div className="status-board-title">
         <h3 className="status-board-title-name">Name</h3>
         <h3 className="status-board-title-status">Status</h3>
       </div>
 
+      {[playerStatus]}
+      {[enemyStatus]}
 
-      <div className="status-board-player">
-        <h3 className="status-board-player-name" >Hero</h3>
-
-        <div className="status-board-player-status">
-          <h4>Sleepy</h4>{" "}
-          <h3>3</h3>
-        </div>
-
-      </div>
-
-      <button onClick={() => setIsStatusBoard(false)} className="status-board-button">Back</button>
+      <button
+        onClick={() => setIsStatusBoard(false)}
+        className="status-board-button"
+      >
+        Back
+      </button>
     </div>
   )
 }
